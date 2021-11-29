2b36f4a8d617ac2d98bfa631d6792fd636f21f68
Абай Молдабеков <abay9508@gmail.com>
2021-11-29 17:44:31 +0600

    Вывод имени через CSS_SELECTOR - h1

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-import requests
-from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
 
-def main():
-    driver = webdriver.Chrome()
-    driver.get('https://www.facebook.com/profile.php?id=100011366594475')
-    full_name = driver.find_element_by_tag_name("html")
-    print(full_name.text)
-
-if __name__ == "__main__":
-    main()
-
-
-
-#insert_resource(source_url, number_of_subscribers, first_name, second_name)
+#This example requires Selenium WebDriver 3.13 or newer
+with webdriver.Chrome() as driver:
+    wait = WebDriverWait(driver, 10)
+    driver.get("https://www.facebook.com/profile.php?id=100011366594475")
+    first_result = wait.until(presence_of_element_located((By.CSS_SELECTOR, "h1")))
+    print(first_result.get_attribute("textContent"))

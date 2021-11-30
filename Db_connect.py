c40067754b43fc8c65da8ec4776f3e44152e94cb
Абай Молдабеков <abay9508@gmail.com>
2021-11-30 11:21:42 +0600

    Первая версия

--- Db_connect.py
+++ Db_connect.py
@@ -10,8 +10,7 @@ try:
                                  source_id bigint(20) NOT NULL AUTO_INCREMENT,
                                  source_url varchar(255) NOT NULL,
                                  number_of_subscribers int(10) NULL,
-                                 first_name varchar(255) NULL,
-                                 second_name varchar(255) NULL,
+                                 full_name varchar(255) NULL,
                                  PRIMARY KEY (source_id)) """
     cursor = connection.cursor()
     result = cursor.execute(mySql_create_table_query)
@@ -28,14 +27,12 @@ finally:
         print("MySQL connection is closed")
 
 
-def insert_resource(source_url, number_of_subscribers, first_name, second_name):
+def insert_resource(source_url, number_of_subscribers, full_name):
     try:
         connection = mysql.connector.connect(**CONNECTION)
-        mySql_insert_resources_query = """INSERT INTO resource (source_url, number_of_subscribers, 
-        first_name, second_name)
-                               VALUES
-                               (%s, %s, %s, %s) """
-        record = (source_url, number_of_subscribers, first_name, second_name)
+        mySql_insert_resources_query = """INSERT INTO resource (source_url, number_of_subscribers, full_name)
+                               VALUES (%s, %s, %s) """
+        record = (source_url, number_of_subscribers, full_name)
         cursor = connection.cursor()
         cursor.execute(mySql_insert_resources_query, record)
         connection.commit()
--- main.py
+++ main.py
@@ -2,11 +2,25 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+
+from Db_connect import insert_resource
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 
-#This example requires Selenium WebDriver 3.13 or newer
+# This example requires Selenium WebDriver 3.13 or newer
+source_url = "https://www.facebook.com/profile.php?id=100011366594475"
 with webdriver.Chrome() as driver:
     wait = WebDriverWait(driver, 10)
-    driver.get("https://www.facebook.com/profile.php?id=100011366594475")
-    first_result = wait.until(presence_of_element_located((By.CSS_SELECTOR, "h1")))
-    print(first_result.get_attribute("textContent"))
+    driver.get(source_url)
+    #first_result = driver.find_element(By.TAG_NAME, "h1")
+    #print(first_result.get_attribute("textContent")) # Абай Молдабеков
+    content = []
+    sub_result = driver.find_elements(By.CLASS_NAME, "d2edcug0")
+    for sub in sub_result:
+        content.append(sub.get_attribute("textContent"))
+
+    full_name = content[11]
+    print(full_name)
+    number_of_subscribers = content[20]
+    print(number_of_subscribers)
+
+insert_resource(source_url, number_of_subscribers, full_name)

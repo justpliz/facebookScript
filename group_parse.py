d15035183107d0c6e084a902df6e2d3a8b609e3a
Абай Молдабеков <abay9508@gmail.com>
2021-11-30 12:53:55 +0600

    Добавлен парсинг групп, но часто вылазит ошибка element is not attached to the page document

--- group_parse.py
+++ group_parse.py
@@ -6,21 +6,20 @@ from selenium.webdriver.support.ui import WebDriverWait
 from Db_connect import insert_resource
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 
+
 # This example requires Selenium WebDriver 3.13 or newer
-source_url = "https://www.facebook.com/people/Абай-Молдабеков/100011366594475/"
+source_url = "https://www.facebook.com/groups/disco80.90"
 with webdriver.Chrome() as driver:
     wait = WebDriverWait(driver, 10)
     driver.get(source_url)
-    #first_result = driver.find_element(By.TAG_NAME, "h1")
-    #print(first_result.get_attribute("textContent")) # Абай Молдабеков
     content = []
     sub_result = driver.find_elements(By.CLASS_NAME, "d2edcug0")
     for sub in sub_result:
         content.append(sub.get_attribute("textContent"))
 
-    full_name = content[11]
+    full_name = content[13]
     print(full_name)
-    number_of_subscribers = content[20]
+    number_of_subscribers = content[20].replace('Участники: ', '')
     print(number_of_subscribers)
 
-insert_resource(source_url, number_of_subscribers, full_name)
+#insert_resource(source_url, number_of_subscribers, full_name)

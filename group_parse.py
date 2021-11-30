b30c587cde8648cd3311680394a8951bdefca0ce
Абай Молдабеков <abay9508@gmail.com>
2021-11-30 14:45:35 +0600

    Кол-во подписчиков отредактированно

--- group_parse.py
+++ group_parse.py
@@ -9,6 +9,7 @@ from selenium.webdriver.support.expected_conditions import presence_of_element_l
 
 # This example requires Selenium WebDriver 3.13 or newer
 source_url = "https://www.facebook.com/groups/disco80.90"
+# https://www.facebook.com/groups/290482172380193
 with webdriver.Chrome() as driver:
     wait = WebDriverWait(driver, 10)
     driver.get(source_url)
@@ -21,5 +22,23 @@ with webdriver.Chrome() as driver:
     print(full_name)
     number_of_subscribers = content[20].replace('Участники: ', '')
     print(number_of_subscribers)
+
+    number_of_subscribers = number_of_subscribers.replace('Участники: ', '')
+    print(number_of_subscribers)
+    if number_of_subscribers.find("тыс.") != -1:
+        number_of_subscribers = number_of_subscribers.replace(' тыс.', '')
+        number_of_subscribers = number_of_subscribers.replace(',', '.')
+        number_of_subscribers = float(number_of_subscribers)
+        number_of_subscribers *= 1000
+        print(number_of_subscribers)
+    elif number_of_subscribers.find(" млн") != -1:
+        number_of_subscribers = number_of_subscribers.replace(' млн', '')
+        number_of_subscribers = number_of_subscribers.replace(',', '.')
+        number_of_subscribers = float(number_of_subscribers)
+        number_of_subscribers *= 1000000
+        print(number_of_subscribers)
+    else:
+        number_of_subscribers = float(number_of_subscribers)
+        print(number_of_subscribers)
 
 #insert_resource(source_url, number_of_subscribers, full_name)

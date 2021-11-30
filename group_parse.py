adba7a82e35834e28a960cf79370bfecd9d60721
Абай Молдабеков <abay9508@gmail.com>
2021-11-30 15:44:25 +0600

    Добавил задержку, которая вроде исправила проблему с устаревшей странией, исправил неправильное представление пробела

--- group_parse.py
+++ group_parse.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+import time
+
 
 from Db_connect import insert_resource
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
@@ -14,25 +16,25 @@ with webdriver.Chrome() as driver:
     wait = WebDriverWait(driver, 10)
     driver.get(source_url)
     content = []
+    time.sleep(1)
     sub_result = driver.find_elements(By.CLASS_NAME, "d2edcug0")
     for sub in sub_result:
         content.append(sub.get_attribute("textContent"))
 
     full_name = content[13]
     print(full_name)
-    number_of_subscribers = content[20].replace('Участники: ', '')
-    print(number_of_subscribers)
-
-    number_of_subscribers = number_of_subscribers.replace('Участники: ', '')
+    subscribers = content[20].replace('Участники: ', '')
+    print(subscribers)
+    number_of_subscribers = subscribers.replace('Участники: ', '')
     print(number_of_subscribers)
     if number_of_subscribers.find("тыс.") != -1:
-        number_of_subscribers = number_of_subscribers.replace(' тыс.', '')
+        number_of_subscribers = number_of_subscribers.replace('\xa0тыс.', '')
         number_of_subscribers = number_of_subscribers.replace(',', '.')
         number_of_subscribers = float(number_of_subscribers)
         number_of_subscribers *= 1000
         print(number_of_subscribers)
     elif number_of_subscribers.find(" млн") != -1:
-        number_of_subscribers = number_of_subscribers.replace(' млн', '')
+        number_of_subscribers = number_of_subscribers.replace('\xa0млн', '')
         number_of_subscribers = number_of_subscribers.replace(',', '.')
         number_of_subscribers = float(number_of_subscribers)
         number_of_subscribers *= 1000000
@@ -41,4 +43,4 @@ with webdriver.Chrome() as driver:
         number_of_subscribers = float(number_of_subscribers)
         print(number_of_subscribers)
 
-#insert_resource(source_url, number_of_subscribers, full_name)
+insert_resource(source_url, number_of_subscribers, full_name)

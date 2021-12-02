333fefac2664a422d9dddcbe34af20eb65b92b0a
Абай Молдабеков <abay9508@gmail.com>
2021-12-02 17:05:37 +0600

    Добавил время, поменял струтуру, поймал новые баги

--- Db_connect.py
+++ Db_connect.py
@@ -1,10 +1,9 @@
 import mysql.connector
-from main import item_content
 from mysql.connector import Error
 
 CONNECTION = {'host': 'localhost', 'database': 'facebookScript', 'user': 'root', 'password': 'MYSQL'}
 connection = mysql.connector.connect(**CONNECTION)
-cursor = connection.cursor()
+
 
 def create_resource_table(cursor):
     try:
@@ -23,40 +22,43 @@ def create_resource_table(cursor):
     except mysql.connector.Error as error:
         print("Failed to create table in MySQL: {}".format(error))
 
-# def insert_resource(CONTENT):
-#     try:
-#         connection = mysql.connector.connect(**CONNECTION)
-#         mySql_insert_resources_query = """INSERT INTO resource (source_url, number_of_subscribers, full_name)
-#                                VALUES (%s, %s, %s) """
-#         record = CONTENT
-#         cursor = connection.cursor()
-#         cursor.execute(mySql_insert_resources_query, record)
-#         connection.commit()
-#         print("Record inserted successfully into resource table")
-#
-#     except mysql.connector.Error as error:
-#         print("Failed to insert into MySQL table {}".format(error))
-
-def update_resource(cursor, item_content):
+def insert_resource(cursor, insert_content):
     try:
-        print("Before updating a record ")
-        sql_select_query = ""f"select * from resource where id = {item_content[0]}"""
-        cursor.execute(sql_select_query)
-        record = cursor.fetchone()
-        print(record)
+        mySql_insert_resources = """INSERT INTO resource (source_url, number_of_subscribers, full_name, 
+        user_id, type, update_date)
+                               VALUES (%s, %s, %s, %s, %s, %s) """
+        cursor.execute(mySql_insert_resources, insert_content)
+        connection.commit()
+        print("Record inserted successfully into resource table")
+
+    except mysql.connector.Error as error:
+        print("Failed to insert into MySQL table {}".format(error))
+
+def update_resource(cursor, update_content):
+    try:
+        mySql_update_resources = """Update resource set source_id = %s, source_url = %s, number_of_subscribers = %s, 
+        full_name = %s, user_id = %s, type = %s, update_date = %s where 
+        source_id = 1"""
+
+        source_id = update_content[0]
+        source_url = update_content[1]
+        number_of_subscribers = update_content[2]
+        full_name = update_content[3]
+        user_id = update_content[4]
+        type = update_content [5]
+        update_date = update_content [6]
+
+        cursor.execute(mySql_update_resources, update_content)
+        connection.commit()
+        print("Record updated successfully into resource table")
+        #record = cursor.fetchone()
+        #print(record)
 
     except mysql.connector.Error as error:
         print("Failed to update table record: {}".format(error))
 
-def get_source_url(source_url):
+def get_source_url(cursor, source_url):
     cursor.execute("SELECT source_url FROM resource")
-    url = cursor.fetchall()[source_url]
+    url = cursor.fetchall()[source_url - 1]
     resource_url = ''.join(map(str, url))
     return resource_url
-
-create_resource_table(cursor)
-
-if connection.is_connected():
-        cursor.close()
-        connection.close()
-        print("MySQL connection is closed")
--- main.py
+++ main.py
@@ -2,16 +2,25 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+from datetime import datetime
 import time
+import mysql.connector
 
 # from Db_connect import insert_resource
+from Db_connect import create_resource_table
 from Db_connect import get_source_url
+from Db_connect import insert_resource
 from Db_connect import update_resource
 
 start_time = time.time()
 # This example requires Selenium WebDriver 3.13 or newer
+
+CONNECTION = {'host': 'localhost', 'database': 'facebookScript', 'user': 'root', 'password': 'MYSQL'}
+connection = mysql.connector.connect(**CONNECTION)
+cursor = connection.cursor()
+create_resource_table(cursor)
 source_id = 1
-source_url = get_source_url(source_id)
+source_url = get_source_url(cursor, source_id)
 
 def group_parse(number_of_subscribers):
     subscribers = number_of_subscribers.replace('Участники: ', '')
@@ -47,14 +56,25 @@ with webdriver.Chrome() as driver:
     print(full_name)
     number_of_subscribers = content[20]
     print(number_of_subscribers)
+    update_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     # print(content)
 
     if source_url.find("/group") != -1:
         number_of_subscribers = group_parse(number_of_subscribers)
 
-item_content = (source_id, source_url, number_of_subscribers, full_name, "test_user_id", "Facebook", "1000-01-02")
 
-update_resource(item_content)
-#insert_resource(CONTENT)
+
+update_content = (source_id, source_url, number_of_subscribers, full_name, "test_user_id", "Facebook", update_date)
+update_resource(cursor, update_content)
+
+insert_content = ('https://www.facebook.com/brigittelindholm.me', '1', 'Абай Молдабеков1', '', 'Facebook', update_date)
+insert_resource(cursor, insert_content)
+
+
+if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed")
+
 
 print("--- %s seconds ---" % (time.time() - start_time))
